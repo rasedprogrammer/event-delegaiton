1fb542dfb55ac9aad88fe8120144718f123863a0
Md. Rased Molla <75846499+rasedprogrammer@users.noreply.github.com>
2021-12-07 22:42:31 +0600

    adding html and js code

--- app.js
+++ app.js
@@ -9,15 +9,22 @@ const tweetForm = document.querySelector("#tweetForm");
 const tweetsContainer = document.querySelector("#tweets");
 tweetForm.addEventListener("submit", function (e) {
   e.preventDefault();
-  const userNameInput = tweetForm.elements.userName;
+  const usernameInput = tweetForm.elements.username;
   const tweetInput = tweetForm.elements.tweet;
-  addTweet(userNameInput.value, tweetInput.value);
-  userNameInput.value = "";
+  addTweet(usernameInput.value, tweetInput.value);
+  usernameInput.value = "";
   tweetInput.value = "";
 });
-const addTweet = (userName, tweet) => {
+
+const addTweet = (username, tweet) => {
   const newTweet = document.createElement("li");
   const bTag = document.createElement("b");
-  bTag.append(userName);
+  bTag.append(username);
   newTweet.append(bTag);
+  newTweet.append(`- ${tweet}`);
+  tweetsContainer.append(newTweet);
 };
+
+tweetsContainer.addEventListener("click", function (e) {
+  e.target.nodeName === "LI" && e.target.remove();
+});
